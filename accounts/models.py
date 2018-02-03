884335351645f862591921d97bc899a4d4b9810c
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-02-03 14:36:21 +0530

    beta-Version-0.0.1

---
+++ accounts/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+from accounts.managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+	full_name=models.CharField(_('full name'), max_length=100)
+	email = models.EmailField(_('email address'), unique=True)
+	phone_no=models.CharField(_('phone no'), max_length=100)
+	avatar = models.ImageField(upload_to='images/avatars/', null=True, blank=True)
+
+	date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+	is_active = models.BooleanField(_('active'), default=True)
+	is_staff = models.BooleanField(_('is_staff'), default=False)
+	is_admin = models.BooleanField(_('is_staff'), default=False)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+
+	class Meta:
+		verbose_name = _('user')
+		verbose_name_plural = _('users')
+
+	def get_full_name(self):
+		'''
+		Returns the first_name plus the last_name, with a space in between.
+		'''
+		full_name = '%s %s' % (self.first_name, self.last_name)
+		return full_name.strip()
+
+	def get_short_name(self):
+		'''
+		Returns the short name for the user.
+		'''
+		return self.first_name
+
+	def email_user(self, subject, message, from_email=None, **kwargs):
+		'''
+		Sends an email to this User.
+		'''
+		send_mail(subject, message, from_email, [self.email], **kwargs)
---
+++ personals/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PersonalsConfig(AppConfig):
+    name = 'personals'
